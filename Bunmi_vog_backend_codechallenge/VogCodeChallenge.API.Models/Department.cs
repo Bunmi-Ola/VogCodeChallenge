eacc71be855a943421ac371e067aa87ceef8cfd7
bolanrewaju <bolanrewaju@cloudhight.com>
2021-08-12 23:16:18 -0600

    question_2 v2.0:  preparation the supporting architecture(classes). Classes were created in a dynamic link library (DLL), allowing them to be used in many other applications

---
+++ Bunmi_vog_backend_codechallenge/VogCodeChallenge.API.Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Department
+    {
+        public int depatment_ID { get; set; }
+        public string depatment_Name { get; set; }
+             
+    }
+}
---
+++ Bunmi_vog_backend_codechallenge/VogCodeChallenge.API.Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Employee
+    {
+        public int empId { get; set; }
+        public string firstName  { get; set; }
+        public string lastName { get; set; }
+        public Job Job { get; set; }
+        public Department Department { get; set; }
+        public string mailing_address { get; set; }
+    }
+}
