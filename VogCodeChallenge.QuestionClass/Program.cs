4f08c5cfc5d652edcd979e33ae6bfbab9c2c59d1
bolanrewaju <bolanrewaju@cloudhight.com>
2021-08-13 09:00:35 -0600

    question 7 v7.0: console project to iterate through the namelist items without using foreach/for loops

---
+++ VogCodeChallenge.QuestionClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VogCodeChallenge.QuestionClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+
+           QuestionClass.DisplayName();           
+        }
+    }
+}
---
+++ VogCodeChallenge.QuestionClass/QuestionClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.QuestionClass
+{
+    public static class QuestionClass
+    {
+      static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static void DisplayName()
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine(NamesList[i]);
+                i++;
+            } while (i < NamesList.Count); ;
+        }
+
+    }
+
+}
