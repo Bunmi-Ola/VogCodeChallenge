de019617418b50a83301ec0a176004691c178224
bolanrewaju <bolanrewaju@cloudhight.com>
2021-08-13 23:14:59 -0600

    question 8 v8.0: TESTModule

--- VogCodeChallenge.QuestionClass/Program.cs
+++ VogCodeChallenge.QuestionClass/Program.cs
@@ -7,7 +7,18 @@ namespace VogCodeChallenge.QuestionClass
         static void Main(string[] args)
         {            
 
-           QuestionClass.DisplayName();           
+           QuestionClass.DisplayName();
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the TESTModule program, enter end to stop");
+            Console.WriteLine("now Enter an input: ");
+            object input = Console.ReadLine();
+            while(input.ToString() != "end")
+            {
+                object value = QuestionClass.TESTModule(input);
+                Console.WriteLine(value);
+                Console.WriteLine("Enter another input: ");
+                input = Console.ReadLine();
+            }
         }
     }
 }
--- VogCodeChallenge.QuestionClass/QuestionClass.cs
+++ VogCodeChallenge.QuestionClass/QuestionClass.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace VogCodeChallenge.QuestionClass
@@ -16,13 +18,48 @@ namespace VogCodeChallenge.QuestionClass
         public static void DisplayName()
         {
             int i = 0;
+            Console.WriteLine("Names in the list includes:");
             do
             {
-                Console.WriteLine(NamesList[i]);
+                Console.WriteLine(i+1 + ". " +NamesList[i]);
                 i++;
             } while (i < NamesList.Count); ;
         }
+                
 
+    public static object TESTModule(object o)
+        {
+            int result; float floatValue;
+            switch (Type.GetTypeCode(o.GetType()))
+            {
+                case TypeCode.String when (int.TryParse(Convert.ToString(o), out result) && 
+                                            (result >= 1) && (result <= 4)):
+                     result = result * 2;                    
+                    return result;
+
+                case TypeCode.String when (int.TryParse(Convert.ToString(o), out result)
+                                            &&(result > 4 )):
+                     result = result * 3;                  
+                    return result;
+
+                case TypeCode.String when (int.TryParse(Convert.ToString(o), out result) 
+                                            && result < 1):
+                    throw new ArgumentOutOfRangeException();
+                                     
+                case TypeCode.String when (float.TryParse(Convert.ToString(o), out floatValue) && 
+                            (floatValue.Equals(1.0f)) || (floatValue.Equals(2.0f))):
+                    floatValue = 3.0f;
+                   return floatValue;
+                   
+                case TypeCode.String:
+
+                    return o.ToString().ToUpper();
+                default:
+                    return o;
+                
+            }
+        }
+      
     }
 
 }
